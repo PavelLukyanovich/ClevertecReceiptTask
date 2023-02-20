bc15b0b79d6744d9ed1d470e5f568e09fd439c94
Pavel Lukyanovich <paellukyanovich1@gmail.com>
2023-02-21 02:24:42 +0300

    test: add test fo product converter

--- receipt/src/test/java/com/clevertec/receipt/utils/ProductConverterTest.java
+++ receipt/src/test/java/com/clevertec/receipt/utils/ProductConverterTest.java
@@ -1,7 +1,24 @@
 package com.clevertec.receipt.utils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.clevertec.receipt.models.dto.ProductDto;
+import com.clevertec.receipt.models.entities.Product;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@RunWith(MockitoJUnitRunner.class)
 class ProductConverterTest {
 
+    @Mock
+    Product product;
+
+    @Test
+    public void convert_WhenProductIsNull_ShouldReturnNull() {
+
+        ProductDto actualProductDto = ProductConverter.convert(null);
+        assertNull(actualProductDto);
+    }
 }
